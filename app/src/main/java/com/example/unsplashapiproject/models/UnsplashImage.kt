08da8902bf926cdc59deb3e1281f98deb452b429
Ajah Emmanuel Nnanna <nnannamarach4@gmail.com>
2025-05-22 23:38:28 +0100

    Started setting up this project. Added get requests for getting photos and searching photos

---
+++ app/src/main/java/com/example/unsplashapiproject/models/UnsplashImage.kt
@@ -0,0 +1,40 @@
+package com.example.unsplashapiproject.models
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.unsplashapiproject.utils.Constants.UNSPLASH_IMAGE_TABLE
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Entity(tableName = UNSPLASH_IMAGE_TABLE)
+data class UnsplashImage(
+    @PrimaryKey(autoGenerate = false)
+    val id: String?,
+    @Embedded
+    val url: Url?,
+    val noOfLikes: Int,
+    @Embedded
+    val user: User
+)
+
+@Serializable
+data class Url(
+    val regular: String?
+)
+
+
+
+@Serializable
+data class User(
+    @SerialName(value = "links")
+    @Embedded
+    val userLinks: UserLinks,
+    val name: String?
+)
+
+
+@Serializable
+data class UserLinks(
+    val htm: String?
+)
---
+++ app/src/main/java/com/example/unsplashapiproject/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.unsplashapiproject.utils
+
+object Constants {
+    const val UNSPLASH_IMAGE_TABLE = "unsplash_image_table"
+}
